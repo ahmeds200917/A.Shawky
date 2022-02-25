bd40e9437773f4fc5c4cc40262033e8702d9c0a9
ahmeds200917 <ahmeds_2009@hotmail.com>
2022-02-25 13:42:29 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,34 +1,38 @@
-#!/bin/sh
+#!/bin/sh # 
+ # # Command: wget https://raw.githubusercontent.com/ahmeds200917/A.Shawky/main/ArabicSavior-1.4.tar.gz -O - | /bin/sh # # ########################################### ###########################################  
+MY_URL="https://raw.githubusercontent.com/ahmeds200917/A.Shawky/main/ArabicSavior-1.4.tar.gz"  
+echo "******************************************************************************************************************"
+echo "download and install ArabicSavior-1.4 mora"
+echo "============================================================================================================================="
 
-version=1.4
-Novaler
-# remove old version
-if [ -f /var/lib/dpkg/status ]; then
-   apt-ger -r enigma2-plugin-extensions-arabicsavior
-else
-   opkg remove enigma2-plugin-extensions-arabicsavior
-fi
-
+#####################################################################################
+echo " download and install ArabicSavior-1.4   "
 cd /tmp
-rm -f *ArabicSavior*
-# Download new version
-wget -O /tmp/ArabicSavior-1.4.tar.gz "https://raw.githubusercontent.com/ahmeds200917/A.Shawky/main/ArabicSavior-1.4.tar.gz"
-
-# remove old version
-rm -r /usr/lib/enigma2/python/Plugins/Extensions/ArabicSavior > /dev/null 2>&1
-
-# Install new version
-tar -xzf /tmp/*.tar.gz -C /
-
-sync
-echo "#########################################################"
-echo "#      ArabicSavior INSTALLED SUCCESSFULLY              #"
-echo "#     mfaraj57  &  RAED  & Ahmed Shawky                 #"              
-echo "#                     support                           #"
-echo "#   https://www.tunisia-sat.com/forums/threads/3896466/ #"
-echo "#########################################################"
-echo "#           your Device will RESTART Now                #"
-echo "#########################################################"
-sleep 3
+wget  "https://raw.githubusercontent.com/ahmeds200917/A.Shawky/main/ArabicSavior-1.4.tar.gz"
+wait
+echo " ArabicSavior-1.4 "
+tar -xzf ArabicSavior-1.4.tar.gz  -C /
+wait
+sleep 2;
+echo "" 
+echo "" 
+echo "****************************************************************************************************************************"
+echo "#   INSTALLED SUCCESSFULLY #"
+echo "
+echo " "*********************************************************" 
+	echo "********************************************************************************"
+echo "   UPLOADED BY  >>>>   Mora "   
+sleep 4;
+	echo '========================================================================================================================='
+###########################################                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo "**********************************************************************************"
+wait
 killall -9 enigma2
 exit 0
+
+
+
+
+
+
