24e2c6ed0d199fae65a7907d116d70183addd367
ahmeds200917 <ahmeds_2009@hotmail.com>
2023-08-24 18:14:37 +0300

    Add files via upload

---
+++ Puretransinstalation .sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+
+wget -O /tmp/pure2trans.tar.gz "https://github.com/ahmeds200917/A.Shawky/blob/main/pure2trans.tar.gz"
+
+tar -xzf /tmp/*.tar.gz -C /
+
+wait
+opkg install --force-overwrite /tmp/*.ipk
+
+rm -r /tmp/pure2trans.tar.gz
+sleep 2;
+
+fi
+
+sync
+echo "#########################################################"
+echo "#      Pure2 translation installed              #"
+echo "#                 By Ahmed Shawky                     #"              
+echo "#                     support                           #"
+echo "#                   Novaler team              #"
+echo "#########################################################"
+echo "#           your Device will RESTART Now                #"
+echo "#########################################################"
+sleep 3
+killall enigma2
+exit 0
+
+
+
+
+
+
+
+
