2eb065d9b7e19d87ae56a5b958aac04c976bdf59
ahmeds200917 <ahmeds_2009@hotmail.com>
2024-10-01 12:44:45 +0300

    Update xtraevent-6.7.sh

--- xtraevent-6.7.sh
+++ xtraevent-6.7.sh
@@ -1,22 +1,4 @@
 #!/bin/sh
-
-
-
-
-echo "#########################################################"
-echo "#########################################################"
-echo "Installing dependency files"
-opkg install python3-codecs" "python3-compression" "python3-core" "python3-difflib" "python3-json" "python3-requests" "python3-xmlrpc
-
-SETTINGS="/etc/enigma2/settings"
-echo "Adding new settings for xtraevent..."
-echo ""
-echo ">>>>>>>>>     RESTARTING     <<<<<<<<<"
-echo ""
-init 4
-sleep 3
-sed -i "/xtraevent/d" $SETTINGS
-{
     echo "config.plugins.xtraEvent.apis=True"
 	echo "config.plugins.xtraEvent.deletFiles=False"
 	echo "config.plugins.xtraEvent.extra3=True"
@@ -31,16 +13,3 @@ sed -i "/xtraevent/d" $SETTINGS
 	echo "config.plugins.xtraEvent.tmdbAPI=c7ca0c239088f1ae72a197d1b4be51b8"
 	echo "config.plugins.xtraEvent.tvdb=True"
         echo "config.plugins.xtraEvent.tvdbAPI=a99d487bb3426e5f3a60dea6d3d3c7ef"
-	
-} >> $SETTINGS
-
-# ============================================================================================================
-sleep 2
-sync
-init 3
-echo "==================================================================="
-echo "===                          FINISHED                           ==="
-echo "                         Modded by AShawky                        ==="
-echo "==================================================================="
-sleep 2
-exit 0
