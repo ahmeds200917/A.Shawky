c5f4bb812cf1b8fcd020850c3a99ea18d83157fb
ahmeds200917 <ahmeds_2009@hotmail.com>
2024-10-01 12:21:17 +0300

    Update xtraevent-6.7.sh

--- xtraevent-6.7.sh
+++ xtraevent-6.7.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-##setup command=wget -q "--no-check-certificate" https://github.com/popking159/ssupport/raw/main/subssupport-install.sh -O - | /bin/sh
 
 echo ''
 echo '************************************************************'
