c5ffd1411ea012a8981883201c38f51254dd62ec
ahmeds200917 <ahmeds_2009@hotmail.com>
2025-08-11 20:32:26 +0300

    Update xtraevent-6.7.sh

--- xtraevent-6.7.sh
+++ xtraevent-6.7.sh
@@ -113,13 +113,13 @@ echo "> Setup the plugin..."
 touch "$temp_dir/temp_file"
 cat <<EOF > "$temp_dir/temp_file"
 config.plugins.xtraEvent.apis=True
+config.plugins.xtraEvent.banner=True
 config.plugins.xtraEvent.deletFiles=False
 config.plugins.xtraEvent.extra3=True
 config.plugins.xtraEvent.FanartSearchType=movies
 config.plugins.xtraEvent.loc=/media/hdd/
 config.plugins.xtraEvent.searchMANUELnmbr=0
 config.plugins.xtraEvent.searchNUMBER=8
-config.plugins.xtraEvent.tmdbAPI=c7ca0c239088f1ae72a197d1b4be51b8
 config.plugins.xtraEvent.searchType=movie
 config.plugins.xtraEvent.timerHour=24
 config.plugins.xtraEvent.timerMod=Period
@@ -127,6 +127,7 @@ config.plugins.xtraEvent.tmdbAPI=c7ca0c239088f1ae72a197d1b4be51b8
 config.plugins.xtraEvent.tvdb=True
 config.plugins.xtraEvent.tvdbAPI=a99d487bb3426e5f3a60dea6d3d3c7ef
 EOF
+
 
 # Update Enigma2 settings
 sed -i "/xtraEvent/d" /etc/enigma2/settings
